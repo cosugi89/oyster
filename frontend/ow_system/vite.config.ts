3d44f78969f80ce41306f4a2d41703a03b5d6d8e
sakai.akira <sakai.akira@oysterworld.jp>
2025-09-10 02:01:02 +0900

    init4

--- frontend/ow_system/vite.config.ts
+++ frontend/ow_system/vite.config.ts
@@ -1,17 +1,21 @@
 import path from "path"; //npm install -D @types/node
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite' //追加(tailwind.css)
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite"; //追加(tailwind.css)
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss() //追加
+    tailwindcss(), //追加
   ],
-  resolve: { //追加(shadcn/ui)
+  resolve: {
+    //追加(shadcn/ui)
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  build: {
+    outDir: "build", // distからbuildに変更
+  },
+});
